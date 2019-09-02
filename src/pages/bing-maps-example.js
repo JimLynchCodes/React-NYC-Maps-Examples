d97db059f8cfb7f699c1ab353f1ba3f1f231a066
JimTheMan <mrdotjim@gmail.com>
2019-09-02 17:30:38 -0400

    initial try for bing maps

--- src/pages/bing-maps-example.js
+++ src/pages/bing-maps-example.js
@@ -1,48 +1,81 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+// let Microsoft;
 
-const BingMapsExamplePage = () => (
-  <Layout>
-    <Link to="/">{'<-'} Back Home</Link>
-    <br/>
-    <SEO title="Bing Maps Example" />
-    <br/>
-    <h1>Bing Maps Example</h1>
-    
-    <h3>Coming Soon...! 😋</h3>
-    {/**  TODO - put bing maps example here ! */}
+const BingMapsExamplePage = () => {
 
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-  </Layout>
-);
+  let map, contextMenu;
+
+  useEffect(() => {
+
+    console.log("bing ", process.env.BING_MAPS_KEY)
+
+    map = new Microsoft.Maps.Map('#myMap', {});
+   
+    Microsoft.Maps.Events.addHandler(map, 'rightclick', function (e) {
+      //Open context menu.
+      contextMenu.setOptions({
+        location: e.location,
+        visible: true
+      });
+      document.body.onmousedown = function () {
+        closeContextMenu();
+      };
+
+      // }, 50)
+      function closeContextMenu() {
+        contextMenu.setOptions({ visible: false });
+      }
+    });
+
+  }, [])
+
+  return (
+
+    < Layout >
+      <Link to="/">{'<-'} Back Home</Link>
+      <br />
+      <SEO title="Bing Maps Example" />
+      <br />
+      <h1>Bing Maps Example</h1>
+
+      <h3>Coming Soon...! 😋</h3>
+
+      <div id="myMap" ></div>
+      {/**  TODO - put bing maps example here ! */}
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </Layout >
+  )
+};
 
 export default BingMapsExamplePage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,9 @@ import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
+    {/* <head> */}
+    <script type='text/javascript' src={`https://www.bing.com/api/maps/mapcontrol?callback=GetMap&key=${process.env.BING_MAPS_KEY}`} ></script>
+    {/* </head> */}
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Welcome to React Maps Examples!</h1>
     <p>The purpose of this little webapp is to show that you can build really nice map components for mobile web browsers in React. :)</p>
